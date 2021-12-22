c3ddf0bd65228eea243355d2b62d9dc069a105f0
Morphine025 <yanxi.0916@gmail.com>
2021-12-22 09:57:51 +0800

    submit workspace by Morphine

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <label for="">
+     <p>Self introduction</p> 
+      <br>
+      <textarea :cols="width" :rows="height" v-model='content' :maxlength="total"></textarea>
+      <br>
+      <p>还能输入{{count}}字</p>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      content:''
+    }
+  },
+  computed:{
+    count(){
+      return this.total -this.content.length;
+    }
+  },
+  props: {
+    total:{
+      type:Number,
+      default:200
+    },
+    title:{
+      type:String,
+      default:'多行文本标题'
+    },
+    width:{
+      type:Number,
+      default:20
+    },
+    height:{
+      type:Number,
+      default:3
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
